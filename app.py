59f870e8a9a8287393a5e021ac21d66e7f907b25
cswclui <218137+cswclui@users.noreply.github.com>
2025-02-09 01:37:40 +0000

    update bob

--- app.py
+++ app.py
@@ -12,8 +12,13 @@ def get_todos():
 @app.route('/todos', methods=['POST'])
 def add_todo():
     todo = request.json
-    todos.append(todo)
-    return jsonify(todo), 201
+
+    # add item only if it is not already present
+    if todo in todos:
+        return jsonify({'error': 'Item already exists'}), 400
+    else:
+        todos.append(todo)
+        return jsonify(todo), 201
 
 @app.route('/todos/<int:todo_id>', methods=['PUT'])
 def update_todo(todo_id):
